3b635061cbba237fce0202023b7d8b1e8b51830b
Rakshith N <rakshithn6485@gmail.com>
2026-01-08 09:57:52 +0530

    Remove static file and added master and quality routes

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,35 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+import os 
+
 
 app = FastAPI()
+BASE_HLS_PATH = os.path.abspath("../hls")
 
-app.mount("/media", StaticFiles(directory="../hls"),name="media")
 @app.get("/")
 def home():
     return {"message": "Streaming Service API is running"}
+
+@app.get("/media/master.m3u8")
+def get_master_playlist():
+    path = os.path.join(BASE_HLS_PATH,"master.m3u8")
+    if not os.path.exists(path):
+        raise HTTPException(status_code=404)
+    return FileResponse(path,media_type="application/vnd/apple.mpegurl")
+
+@app.get("/media/{quality}/{filename}")
+def get_hls_file(quality : str,filename: str):
+    path = os.path.join(BASE_HLS_PATH,quality,filename)
+    if not os.path.exists(path):
+        raise HTTPException(status_code=404)
+    if filename.endswith(".m3u8"):
+        media_type = "application/vnd.apple.mpegurl"
+    else:
+        media_type="video/MP2T"
+        
+    return FileResponse(path,media_type = media_type)
+
+
+
+
   

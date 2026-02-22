5089db9f8218ac9196fa067f15866ae55d8bbde8
Rakshith N <rakshithn6485@gmail.com>
2026-02-22 13:24:31 +0530

    added expiry access token and refresh acess token to unexpire

--- backend/auth/routes.py
+++ backend/auth/routes.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter , HTTPException
 from jose import JWTError, jwt
 from pydantic import BaseModel 
-from config import SECRET_KEY , ALGORITHM
-
+from config import SECRET_KEY , ALGORITHM , ACCESS_TOKEN_EXPIRE_MINUTES, REFRESH_TOKEN_EXPIRE_DAYS
+from datetime import datetime, timedelta
 router = APIRouter()
 
 users = {
@@ -16,15 +16,62 @@ class LoginRequest(BaseModel):
     username : str
     password : str
     
+    
 @router.post("/login")
 def login(req : LoginRequest):
     user = users.get(req.username)
     if not user or user["password"] != req.password:
       raise HTTPException(status_code= 401, detail="Invalid username or password")
+    #expire token after 30 minutes
+    access_expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     
-    token_data = {
+    access_payload = {
       "username": req.username,
-      "max_quality": user["max_quality"]
+      "max_quality": user["max_quality"],
+      "type": "access",
+      "exp": access_expire
     }
-    token = jwt.encode(token_data, SECRET_KEY , algorithm=ALGORITHM)
-    return {"access token": token}
+    access_token = jwt.encode(access_payload, SECRET_KEY , algorithm=ALGORITHM)
+    
+    #refresh token
+    refresh_expire = datetime.utcnow() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    refresh_payload = {
+      "username": req.username,
+      "type": "refresh",
+      "exp": refresh_expire
+    } 
+    refresh_token = jwt.encode(refresh_payload, SECRET_KEY , algorithm=ALGORITHM)
+    
+    return {"access token": access_token, "refresh token": refresh_token}
+  
+#create reqmodel
+
+class RefreshRequest(BaseModel):
+    refresh_token: str
+
+  
+@router.post("/refresh")
+def refresh_token(request: RefreshRequest):
+    try:
+        payload = jwt.decode(request.refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
+        if payload.get("type")!="refresh":
+            raise HTTPException(status_code=401, detail="Invalid token type")
+        
+        access_expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        new_access_playload = {
+            "username" : payload["username"],
+            "max_quality": users[payload["username"]]["max_quality"],
+            "type": "accesss",
+            "exp": access_expire
+            
+        }
+        new_access_token = jwt.encode(new_access_playload, SECRET_KEY, algorithm=ALGORITHM)
+        return {"access_token": new_access_token}
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid or expired refresh token")
+        
+          
+    
+          
+  
+    
--- backend/config.py
+++ backend/config.py
@@ -5,3 +5,5 @@ import os
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
